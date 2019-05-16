0841968e99cd1d803e97e126e37435ddfac80d2b
juancarloslg <juancarlos@multicursos.com.mx>
2019-05-16 18:10:48 -0500

    Subo contacto y plataformas

---
+++ contacto.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Muestra Resellers</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../favicon.ico">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+      <?php include "templates/header.php"; ?>
+<!-- NAVBAR
+================================================== -->
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row ">
+        <div  class="card col-lg-4">
+        <a style="font-size: 100%" href=“#“>
+          <i class="fas fa-8x fa-phone-square"></i>
+          <h2>Teléfono</h2>
+          
+          <p>(55) 5710 5865</a> </p>
+        </div><!-- /.col-lg-4 -->
+        
+        <div class="card col-lg-4">
+        <a href=“#” target=“_blank”>
+          <i class="fas fa-8x  fa-building"></i>
+          <h2>Nuestra oficina</h2>
+          <p>Calderon de la Barca. Polanco. CDMX</p>
+          </a>
+        </div><!-- /.col-lg-4 -->
+        
+        <div class="card col-lg-4">
+            <a href=“#”>
+              <i class="fas fa-8x fa-envelope"></i>
+              <h2>Mail</h2>
+              <p>informes@multicursos.com.mx</p>
+              </p>
+            </a>
+        </div><!-- /.col-lg-4 -->
+        
+      </div><!-- /.row -->
+
+
+  <?php include "templates/footer.php"; ?>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+  <script type="text/javascript" src="https://www.alsacreations.com/xmedia/tuto/exemples/parallaxe/etape3/script/jquery.parallax-1.1.js"></script>
+  
+</body>
+  
+  
+
+    <script  src="js/index.js"></script>
+
+</html>
---
+++ plataformas.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=gb18030">
+  
+  <title>A Pen by  Demonolog</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+
+  
+  <head>
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Carousel Template for Bootstrap</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+
+
+    <![endif]-->
+
+  <style type="text/css">
+    
+img {
+  border-radius: 3%;
+}
+  </style>
+
+
+
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+   <?php include "templates/header.php"; ?>
+
+
+    <!-- Carousel
+    ================================================== -->
+<!-- /.carousel -->
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+    <!-- /.row -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Mendi Academy.</h2>
+          <p class="lead">En esta plataforma vas a poder ver videos hechos especiales para que puedas complementar tus conocimientos.</p>
+          
+          <a href="http://mendiacademy.com.mx/nethome/login.php" class="btn btn-info" role="button">Entrar</a>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="img/mendi.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading">2LOQUER.</h2>
+          <p class="lead">En esta plataforma podr谩s ver el contenido de tus materias, tus evaluaciones y la retroalimentaci贸n de tus actividades.</p>   
+          <a href="http://2loquer.mx/moodle/" class="btn btn-info" role="button">Entrar</a>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" src="img/Logo2Loquer.jpg" alt="Generic placeholder image">
+
+
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">WIZIQ.</h2>
+          <p class="lead">En esta plataforma vas a poder tomar tus sesiones dependiendo de tu plan.</p>
+          <a href="https://www.wiziq.com/es/" class="btn btn-info" role="button">Entrar</a>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="img/wiziq.jpg" alt="Generic placeholder image"> 
+        </div>
+      </div>
+
+
+      <!-- /END THE FEATURETTES -->
+
+      <!-- FOOTER -->
+     <?php include "templates/footer.php"; ?>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+  <script type="text/javascript" src="https://www.alsacreations.com/xmedia/tuto/exemples/parallaxe/etape3/script/jquery.parallax-1.1.js"></script>
+  </body>
+  
+  
+
+    <script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
